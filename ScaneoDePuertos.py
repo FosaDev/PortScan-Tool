58a24734f9eb87de80bc0fb161edebc3d1c81a82
FosaDev <devfosa@gmail.com>
2022-02-02 19:34:23 -0500

    Subida de la herramineta

---
+++ ScaneoDePuertos.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+
+
+def RevisionDePuertos(ip, ListaDePuertos):
+    try:
+        for Puertos in ListaDePuertos:
+            Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM,)
+            Socket.settimeout(5)
+            Resultado = Socket.connect_ex((ip, Puertos))
+            if Resultado == 0:
+                print("Puerto {}: \t Abierto".format(Puertos))
+            Socket.close()
+    except Socket.error as error:
+        print(str(error))
+        print("La conexion a fallado")
+        sys.exit()
+
+DireccionIp = input("Digite la direccion Ip: ")
+for i in range(65535):
+    i += 1
+    RevisionDePuertos(DireccionIp, [i, i, i])
